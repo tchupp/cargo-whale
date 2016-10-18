93e8c81c41eff123d10cff6f916b78ee7712f96c
tclchiam <tclchiam@gmail.com>
2016-10-18 00:38:09 -0400

    Started using default profile utility, added logging to main to display port access

--- src/main/java/com/cargowhale/docker/CargoWhaleDockerApplication.java
+++ src/main/java/com/cargowhale/docker/CargoWhaleDockerApplication.java
@@ -1,12 +1,31 @@
 package com.cargowhale.docker;
 
+import com.cargowhale.docker.config.DefaultProfileUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @SpringBootApplication
 public class CargoWhaleDockerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CargoWhaleDockerApplication.class, args);
-	}
+    private static final Logger log = LoggerFactory.getLogger(CargoWhaleDockerApplication.class);
+
+    public static void main(String[] args) throws UnknownHostException {
+        SpringApplication app = new SpringApplication(CargoWhaleDockerApplication.class);
+        DefaultProfileUtil.addDefaultProfile(app);
+        Environment env = app.run(args).getEnvironment();
+        log.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\thttp://localhost:{}\n\t" +
+                        "External: \thttp://{}:{}\n----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                env.getProperty("server.port"),
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"));
+    }
 }
