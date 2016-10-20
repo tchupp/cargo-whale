707dd42ba00330cdb2f85a69a4662bec53769fce
Nick <nicholas.reuter2013@gmail.com>
2016-10-19 23:25:29 -0400

    Adds endpoints for listing all containers and listing all containers with a specific status

--- src/main/java/com/cargowhale/docker/controller/DockerController.java
+++ src/main/java/com/cargowhale/docker/controller/DockerController.java
@@ -3,10 +3,13 @@ package com.cargowhale.docker.controller;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
 
 @RestController
 public class DockerController {
@@ -22,4 +25,17 @@ public class DockerController {
     public String getImages() {
         return this.restTemplate.getForEntity(this.socatUri + "/images/json", String.class).getBody();
     }
+
+    @RequestMapping(value = "/get/containers", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String getAllContainers() {
+        return this.restTemplate.getForEntity(this.socatUri + "/containers/json?all=1", String.class).getBody();
+    }
+
+    //Filter options are: [created, restarting, running, paused, exited, dead]
+    @RequestMapping(value = "/get/containers/{filter}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String getFilteredContainers(@PathVariable("filter") String filter) {
+        String filterString = "{\"status\":[\""+filter+"\"]}";
+        return this.restTemplate.getForEntity(this.socatUri + "/containers/json?filters={filterString}", String.class, filterString).getBody();
+    }
+
 }
