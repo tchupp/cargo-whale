5e8abf56860d0fe5789982a703a16f0e2ec2d851
tclchiam <tchupp@pillartechnology.com>
2016-12-26 20:03:37 -0500

    Changed the breadcrums on the header to follow the current path

--- src/main/webapp/app/layouts/header/header.component.ts
+++ src/main/webapp/app/layouts/header/header.component.ts
@@ -1,8 +1,24 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {Router} from "@angular/router";
 
 @Component({
     selector: 'cw-header',
     templateUrl: 'app/layouts/header/header.html'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+    private segments: string[];
+    private activeSegment: string;
+
+    constructor(private router: Router) {
+        this.activeSegment = '';
+    }
+
+    ngOnInit(): void {
+        this.router.events.subscribe(() => {
+            const split = this.router.url.split('/');
+            this.segments = split.slice(1, split.length - 1);
+            this.activeSegment = split[split.length - 1];
+        });
+    }
 }
