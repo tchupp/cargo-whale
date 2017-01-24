e05f592335ff162ccd23a8f61e82517bc536966e
tclchiam <tchupp@pillartechnology.com>
2017-01-24 17:01:47 -0500

    CW-67: Added stub config section for UI

--- src/main/webapp/app/pages/containers/containers.routes.ts
+++ src/main/webapp/app/pages/containers/containers.routes.ts
@@ -2,6 +2,7 @@ import {Routes, RouterModule} from "@angular/router";
 import {ContainersComponent} from "./containers.component";
 import {ContainerIndexComponent} from "./components/index/container-index.component";
 import {ContainerDetailsComponent} from "./components/details/container-details.component";
+import {ContainerConfigComponent} from "./components/details/components/config/container-config.component";
 import {ContainerEnvironmentComponent} from "./components/details/components/environment/environment.component";
 import {ContainerNetworkComponent} from "./components/details/components/networks/networks.component";
 import {ContainerDetailsResolver} from "./components/details/container-details.resolver";
@@ -22,6 +23,7 @@ const routes: Routes = [{
             },
             children: [
                 {path: '', redirectTo: 'environment', pathMatch: 'full'},
+                {path: 'config', component: ContainerConfigComponent},
                 {path: 'environment', component: ContainerEnvironmentComponent},
                 {path: 'network', component: ContainerNetworkComponent}
             ]
