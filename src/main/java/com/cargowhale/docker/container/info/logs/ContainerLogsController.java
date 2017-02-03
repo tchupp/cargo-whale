dce4bc479b5ce4844b83424cf9d905710b23bfa5
Nicholas Reuter <nicholas.reuter2013@gmail.com>
2017-02-02 21:33:09 -0500

    CW-68: starts adding stuff for websockets

--- src/main/java/com/cargowhale/docker/container/info/logs/ContainerLogsController.java
+++ src/main/java/com/cargowhale/docker/container/info/logs/ContainerLogsController.java
@@ -2,6 +2,8 @@ package com.cargowhale.docker.container.info.logs;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,13 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/containers")
 public class ContainerLogsController {
 
+    private SimpMessagingTemplate messageSender;
     private final ContainerLogsService logsService;
     private final ContainerLogsResourceAssembler logsResourceAssembler;
 
     @Autowired
-    public ContainerLogsController(final ContainerLogsService logsService, final ContainerLogsResourceAssembler logsResourceAssembler) {
+    public ContainerLogsController(final ContainerLogsService logsService, final ContainerLogsResourceAssembler logsResourceAssembler, SimpMessagingTemplate messageSender) {
         this.logsService = logsService;
         this.logsResourceAssembler = logsResourceAssembler;
+        this.messageSender = messageSender;
     }
 
     @RequestMapping(value = "/{id}/logs",
@@ -26,5 +30,11 @@ public class ContainerLogsController {
     public ContainerLogsResource getContainerLogsById(@PathVariable final String id, final LogFilters logFilters) {
         ContainerLogs containerLogs = this.logsService.getContainerLogsById(id, logFilters);
         return this.logsResourceAssembler.toResource(containerLogs);
+    }
+
+    @MessageMapping("/hello")
+    public void greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        messageSender.convertAndSend("/topic/" + message.getRoom(), new Greeting("Hello, " + message.getName() + "!"));
     }
 }
