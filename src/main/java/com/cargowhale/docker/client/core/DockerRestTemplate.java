410ad64d16c48f245cc8cf5ae62a8b238ffe98a7
tclchiam <tchupp@pillartechnology.com>
2017-03-06 00:17:09 -0500

    CW-68: Made LogStreamMessageConverter non-blocking, along with DockerRestTemplate change to make it non-blocking

--- src/main/java/com/cargowhale/docker/client/core/DockerRestTemplate.java
+++ src/main/java/com/cargowhale/docker/client/core/DockerRestTemplate.java
@@ -4,10 +4,15 @@ import org.springframework.boot.web.client.RootUriTemplateHandler;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.client.ClientHttpRequest;
 import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.util.Assert;
+import org.springframework.web.client.*;
 
+import java.io.IOException;
+import java.net.URI;
 import java.util.List;
 
 public class DockerRestTemplate extends RestTemplate {
@@ -21,5 +26,31 @@ public class DockerRestTemplate extends RestTemplate {
 
     public <T> T getForObject(final String url, final ParameterizedTypeReference<T> responseType, final Object... uriVariables) {
         return exchange(url, HttpMethod.GET, HttpEntity.EMPTY, responseType, uriVariables).getBody();
+    }
+
+    @Override
+    protected <T> T doExecute(final URI url, final HttpMethod method, final RequestCallback requestCallback, final ResponseExtractor<T> responseExtractor) throws RestClientException {
+        Assert.notNull(url, "'url' must not be null");
+        Assert.notNull(method, "'method' must not be null");
+
+        ClientHttpResponse response;
+        try {
+            ClientHttpRequest request = createRequest(url, method);
+            if (requestCallback != null) {
+                requestCallback.doWithRequest(request);
+            }
+            response = request.execute();
+            handleResponse(url, method, response);
+            if (responseExtractor != null) {
+                return responseExtractor.extractData(response);
+            } else {
+                return null;
+            }
+        } catch (final IOException ex) {
+            String resource = url.toString();
+            String query = url.getRawQuery();
+            resource = (query != null ? resource.substring(0, resource.indexOf(query) - 1) : resource);
+            throw new ResourceAccessException("I/O error on " + method.name() + " request for \"" + resource + "\": " + ex.getMessage(), ex);
+        }
     }
 }
