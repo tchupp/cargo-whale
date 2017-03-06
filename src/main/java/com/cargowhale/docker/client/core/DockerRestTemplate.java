109f0263a158679a5e32e801c85f7a70af511f3c
tclchiam <tchupp@pillartechnology.com>
2017-03-06 08:04:08 -0500

    CW-68: User can now view logs of containers on the logs tab on the UI

--- src/main/java/com/cargowhale/docker/client/core/DockerRestTemplate.java
+++ src/main/java/com/cargowhale/docker/client/core/DockerRestTemplate.java
@@ -1,23 +1,56 @@
 package com.cargowhale.docker.client.core;
 
+import org.springframework.boot.web.client.RootUriTemplateHandler;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.client.ClientHttpRequest;
 import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.util.Assert;
+import org.springframework.web.client.*;
 
+import java.io.IOException;
+import java.net.URI;
 import java.util.List;
 
 public class DockerRestTemplate extends RestTemplate {
 
-    public DockerRestTemplate(final DockerErrorHandler errorHandler, final ClientHttpRequestFactory requestFactory, final List<HttpMessageConverter<?>> messageConverters) {
+    public DockerRestTemplate(final DockerErrorHandler errorHandler, final ClientHttpRequestFactory requestFactory, final List<HttpMessageConverter<?>> messageConverters, final RootUriTemplateHandler uriTemplateHandler) {
         super(requestFactory);
         setErrorHandler(errorHandler);
         setMessageConverters(messageConverters);
+        setUriTemplateHandler(uriTemplateHandler);
     }
 
     public <T> T getForObject(final String url, final ParameterizedTypeReference<T> responseType, final Object... uriVariables) {
         return exchange(url, HttpMethod.GET, HttpEntity.EMPTY, responseType, uriVariables).getBody();
+    }
+
+    @Override
+    protected <T> T doExecute(final URI url, final HttpMethod method, final RequestCallback requestCallback, final ResponseExtractor<T> responseExtractor) throws RestClientException {
+        Assert.notNull(url, "'url' must not be null");
+        Assert.notNull(method, "'method' must not be null");
+
+        ClientHttpResponse response;
+        try {
+            ClientHttpRequest request = createRequest(url, method);
+            if (requestCallback != null) {
+                requestCallback.doWithRequest(request);
+            }
+            response = request.execute();
+            handleResponse(url, method, response);
+            if (responseExtractor != null) {
+                return responseExtractor.extractData(response);
+            } else {
+                return null;
+            }
+        } catch (final IOException ex) {
+            String resource = url.toString();
+            String query = url.getRawQuery();
+            resource = (query != null ? resource.substring(0, resource.indexOf(query) - 1) : resource);
+            throw new ResourceAccessException("I/O error on " + method.name() + " request for \"" + resource + "\": " + ex.getMessage(), ex);
+        }
     }
 }
--- src/main/java/com/cargowhale/docker/container/info/logs/ContainerLogsClient.java
+++ src/main/java/com/cargowhale/docker/container/info/logs/ContainerLogsClient.java
@@ -3,11 +3,12 @@ package com.cargowhale.docker.container.info.logs;
 import com.cargowhale.docker.client.core.DockerEndpointBuilder;
 import com.cargowhale.docker.client.core.DockerRestTemplate;
 import com.cargowhale.docker.client.core.QueryParameters;
+import com.spotify.docker.client.LogStream;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.util.UriComponentsBuilder;
 
-@Component
+@Repository
 public class ContainerLogsClient {
 
     private final DockerRestTemplate restTemplate;
@@ -19,10 +20,10 @@ public class ContainerLogsClient {
         this.endpointBuilder = endpointBuilder;
     }
 
-    ContainerLogs getContainerLogs(final String containerId, final QueryParameters filters) {
+    LogStream getContainerLogStream(final String containerId, final QueryParameters filters) {
         String containerLogEndpoint = this.endpointBuilder.getContainerLogsEndpoint(containerId);
         UriComponentsBuilder builder = UriComponentsBuilder.fromPath(containerLogEndpoint).queryParams(filters.asQueryParameters());
 
-        return new ContainerLogs(containerId, this.restTemplate.getForObject(builder.toUriString(), String.class));
+        return this.restTemplate.getForObject(builder.toUriString(), LogStream.class);
     }
 }
