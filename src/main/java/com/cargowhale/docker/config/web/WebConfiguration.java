cabdba883dbb25b1aa27fc038cb65fe753891fd0
Theo Chupp <tclchiam@gmail.com>
2017-07-09 15:13:06 -0400

    CW-65: Added config to print dates as ISO8601 in json

--- src/main/java/com/cargowhale/docker/config/web/WebConfiguration.java
+++ src/main/java/com/cargowhale/docker/config/web/WebConfiguration.java
@@ -1,6 +1,9 @@
 package com.cargowhale.docker.config.web;
 
 import com.cargowhale.docker.client.logs.LogStreamMessageConverter;
+import com.fasterxml.jackson.databind.util.ISO8601DateFormat;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -13,5 +16,10 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
     @Override
     public void extendMessageConverters(final List<HttpMessageConverter<?>> converters) {
         converters.add(new LogStreamMessageConverter());
+    }
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer() {
+        return builder -> builder.dateFormat(new ISO8601DateFormat());
     }
 }
